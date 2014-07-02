0fb0b23df3a6a19b46b73ec8bea6de0835bc3bf6
blackbeans.zc <blackbeans.zc@gmail.com>
2014-07-02 23:26:39 +0800

    修改算法 	modified:   com.blackbeans.geohash/core/util.go 	modified:   com.blackbeans.geohash/main/main.go 	com.blackbeans.geohash/main/main

--- com.blackbeans.geohash/core/util.go
+++ com.blackbeans.geohash/core/util.go
@@ -2,7 +2,9 @@ package core
 
 import (
 	"container/list"
+	// "fmt"
 	"math"
+	"strconv"
 )
 
 const (
@@ -20,26 +22,35 @@ func clip(val float64, minVal float64, maxVal float64) float64 {
 	return math.Min(maxVal, math.Max(val, minVal))
 }
 
+func groundResolution(lat float64, level int) float64 {
+	lat = clip(lat, minLat, maxLat)
+	return math.Cos(lat*math.Pi/180) * 2 * math.Pi * radius / float64(mapsize(level))
+}
+
+func mapsize(level int) uint {
+	return uint(256) << uint(level)
+}
+
 /**
  * 这里将经纬度转换成格子的xy坐标
  */
-func LatLng2Tile(lat float64, lng float64, level int) (tileX uint32, tileY uint32) {
-	lat = math.Min(maxLat, math.Max(lat, minLat))
-	lng = math.Min(maxLng, math.Max(lng, minLng))
+func LatLng2Tile(lat float64, lng float64, level int) (tileX int, tileY int) {
+	lat = clip(lat, minLat, maxLat)
+	lng = clip(lng, minLng, maxLng)
 	//投影为平面坐标
 	x := (lng + 180) / 360
 	sval := math.Sin(lat * math.Pi / 180)
 	y := 0.5 - math.Log((1+sval)/(1-sval))/(4*math.Pi)
 
 	//算出总共该的像总素数
-	blockSize := float64(uint32(256 << uint(level)))
+	blockSize := mapsize(level)
 
 	//求取块中的像素坐标
-	pixelX := clip(x*blockSize+0.5, 0, blockSize-1)
-	pixelY := clip(y*blockSize+0.5, 0, blockSize-1)
+	pixelX := int(clip(x*float64(blockSize)+0.5, 0, float64(blockSize)-1))
+	pixelY := int(clip(y*float64(blockSize)+0.5, 0, float64(blockSize)-1))
 
-	tileX = uint32(pixelX / 256)
-	tileY = uint32(pixelY / 256)
+	tileX = int(pixelX / 256)
+	tileY = int(pixelY / 256)
 	return
 
 }
@@ -50,28 +61,36 @@ func LatLng2Tile(lat float64, lng float64, level int) (tileX uint32, tileY uint3
  * 并以4进制的形式输出
  * 用于生成tile索引前缀
  */
-func GenBlockKey(a uint32, b uint32, level int) uint32 {
+func GenBlockKey(a int, b int, level int) string {
 
 	stack := list.New()
-	for i := 0; i < level; i++ {
+	for i := level; i > 0; i-- {
 
-		var bita uint32 = a & 0x01
-		var bitb uint32 = b & 0x01
+		// var bita uint32 = a & 0x01
+		// var bitb uint32 = b & 0x01
 
-		a = a >> 1
-		b = b >> 1
-		c := (bita<<1 | bitb) % 4
+		digit := 0
+		mask := int(1 << uint(i-1))
 
-		stack.PushFront(c)
+		if (a & mask) != 0 {
+			digit++
+		}
+
+		if (b & mask) != 0 {
+			digit++
+			digit++
+		}
+
+		stack.PushBack(digit)
 	}
 
-	quadkey := uint32(0)
+	quadkey := ""
 
 	//翻转显示
 	for stack.Len() > 0 {
 		head := stack.Front()
-		val := head.Value.(uint32)
-		quadkey = quadkey*10 + val
+		val := head.Value.(int)
+		quadkey += strconv.Itoa(val)
 		stack.Remove(head)
 	}
 
--- com.blackbeans.geohash/main/main.go
+++ com.blackbeans.geohash/main/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"com.blackbeans.geohash/core"
 	"fmt"
+	"latlng2pixel/com.blackbeans.geohash/core"
+	"net/http"
+	"strconv"
 )
 
 var blcokMap map[uint32]*core.BlockRange = make(map[uint32]*core.BlockRange)
-var initLevel int = 10
+var initLevel int = 12
 
 var lowestLevel int = initLevel
 
@@ -16,47 +18,74 @@ const (
 
 func main() {
 
-	UpdateLocation(&core.LocationEntry{MomoId: "a", GeoLoc: *&core.Location{0, 90}})
-	valB := UpdateLocation(&core.LocationEntry{MomoId: "b", GeoLoc: *&core.Location{2.4123456, 90.323}})
-	UpdateLocation(&core.LocationEntry{MomoId: "c", GeoLoc: *&core.Location{2.4123453, 90.32300001}})
-	UpdateLocation(&core.LocationEntry{MomoId: "d", GeoLoc: *&core.Location{33, 121.12}})
-	block := UpdateLocation(&core.LocationEntry{MomoId: "e", GeoLoc: *&core.Location{2.4123455, 90.3230000011}})
-	fmt.Println(valB.IdxKey)
-	for _, p := range block.Peoples {
-		fmt.Println(p.MomoId)
-	}
+	// UpdateLocation(&core.LocationEntry{MomoId: "a", GeoLoc: *&core.Location{0, 90}})
+	// valB := UpdateLocation(&core.LocationEntry{MomoId: "b", GeoLoc: *&core.Location{2.4123456, 90.323}})
+	// UpdateLocation(&core.LocationEntry{MomoId: "c", GeoLoc: *&core.Location{2.4123453, 90.32300001}})
+	// UpdateLocation(&core.LocationEntry{MomoId: "d", GeoLoc: *&core.Location{33, 121.12}})
+	// block := UpdateLocation(&core.LocationEntry{MomoId: "e", GeoLoc: *&core.Location{2.4123455, 90.3230000011}})
+	// fmt.Println(valB.IdxKey)
+	// for _, p := range block.Peoples {
+	// 	fmt.Println(p.MomoId)
+	// }
 
-	fmt.Println(block.IdxKey)
+	// x, y := core.LatLng2Tile(40.17887331434695, 116.3671875, initLevel)
+	// idkey := core.GenBlockKey(x, y, int(initLevel))
+
+	http.HandleFunc("/q", func(resp http.ResponseWriter, req *http.Request) {
+
+		latV := req.FormValue("lat")
+		lngV := req.FormValue("lng")
+
+		lat, _ := strconv.ParseFloat(latV, 9)
+
+		lng, _ := strconv.ParseFloat(lngV, 9)
+
+		x, y := core.LatLng2Tile(lat, lng, initLevel)
+		idkey := core.GenBlockKey(x, y, initLevel)
+
+		val := "{\"key\":\"" + idkey + "\"}"
+
+		resp.Header().Set("content-type", "text/json")
+
+		resp.Write([]byte(val))
+
+		defer req.Body.Close()
+
+		fmt.Println(val)
+
+	})
+
+	http.ListenAndServe(":8999", nil)
 
 }
 
-func UpdateLocation(entry *core.LocationEntry) (tile *core.BlockRange) {
+// func UpdateLocation(entry *core.LocationEntry) (tile *core.BlockRange) {
 
-	startLevel := initLevel
-	//这里根据这个块中的数据的${maxBlockPeople}进行下次分裂
-	x, y := core.LatLng2Tile(entry.GeoLoc.Lat, entry.GeoLoc.Lng, startLevel)
-	//获取坐标块的key
-	idxKey := core.GenBlockKey(y, x, int(startLevel))
+// 	startLevel := initLevel
+// 	//这里根据这个块中的数据的${maxBlockPeople}进行下次分裂
+// 	x, y := core.LatLng2Tile(entry.GeoLoc.Lat, entry.GeoLoc.Lng, startLevel)
+// 	//获取坐标块的key
+// 	idxKey := core.GenBlockKey(y, x, int(startLevel))
 
-	//判断是否存在该块，存在判断是否该用户的location存在
+// 	//判断是否存在该块，存在判断是否该用户的location存在
 
-	val, ok := blcokMap[idxKey]
-	if !ok {
-		//该块刚建立，则需要将用户写入即可
-		val = &core.BlockRange{TileX: x, TileY: y, IdxKey: idxKey, Peoples: make(map[string]*core.LocationEntry)}
-		blcokMap[idxKey] = val
-		val.Peoples[entry.MomoId] = entry
-	} else {
-		_, exists := val.Peoples[entry.MomoId]
-		//如果这个用户不存在并且当前块中的人数大于等于块中最大人数
-		//则分裂
-		if !exists && len(val.Peoples) >= maxBlockPeople {
+// 	val, ok := blcokMap[idxKey]
+// 	if !ok {
+// 		//该块刚建立，则需要将用户写入即可
+// 		val = &core.BlockRange{TileX: x, TileY: y, IdxKey: idxKey, Peoples: make(map[string]*core.LocationEntry)}
+// 		blcokMap[idxKey] = val
+// 		val.Peoples[entry.MomoId] = entry
+// 	} else {
+// 		_, exists := val.Peoples[entry.MomoId]
+// 		//如果这个用户不存在并且当前块中的人数大于等于块中最大人数
+// 		//则分裂
+// 		if !exists && len(val.Peoples) >= maxBlockPeople {
 
-		} else {
-			//更新人的位置
-			val.Peoples[entry.MomoId] = entry
-		}
-	}
+// 		} else {
+// 			//更新人的位置
+// 			val.Peoples[entry.MomoId] = entry
+// 		}
+// 	}
 
-	return val
-}
+// 	return val
+// }
